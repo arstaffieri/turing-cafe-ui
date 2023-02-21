f7fc19352bac6de5f4f8904492460ca828d1300a
Angie Staffieri <angie.staffieri@gmail.com>
2023-02-21 11:43:52 -0700

    added form

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Reservations from '../Reservations/Reservations.js';
 import { getAllReservations } from '../apiCalls';
+import Form from '../Form/Form';
 
 class App extends Component {
   constructor() {
@@ -18,11 +19,16 @@ class App extends Component {
       })
   }
 
+  addReservation = (newReservation) => {
+    this.setState({reservations: [...this.state.reservations, newReservation]})
+  }
+
 
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <Form addReservation={this.addReservation}/>
         <div className='resy-form'>
           <Reservations reservations={this.state.reservations}/>
         </div>
